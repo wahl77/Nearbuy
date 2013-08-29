80d730636d6fe2c7657fb7a3bd736f1b82a6a594
Franky W. (franklinwahl - Frankys-MacBook-Pro.local) <wahl77+git@gmail.com>
2013-08-29 11:16:21 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nearbuy::Application.config.secret_key_base = '9cafc6330c2984823c8d95e676762c13e8ee9f57ca56112a2ead449487c56e455d54eb4f3171f7ae73431d7b02b69e6a188cc64a19b73e934101199bb6645c09'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nearbuy::Application.config.session_store :cookie_store, key: '_Nearbuy_session'
