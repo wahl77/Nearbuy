3d21322e460d0ff0a650a1836a4ba34df7d7b4da
Franky W. (franklinwahl - Frankys-MacBook-Pro.local) <wahl77+git@gmail.com>
2013-09-10 11:50:03 -0700

    Added some seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,24 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.destroy_all
 Country.destroy_all
 Category.destroy_all
+
 %w[Switzerland USA].each do |x|
   Country.create!(name: x)
 end
+
+address = Address.create!(number_and_street: "414 Brannan Street", city: "San Francisco", zip_code: "94107", state: "CA", country: Country.find_by_name("Usa"))
 
 %w( General Rentals Neighborliness Travel Education Pets Events Jobs Recommendations Restaurant Bar ).each do |x| 
   Category.create(name: x)
 end
 
 
-a = User.create(email: "a@a.com", password: "a")
+# Data 
+a = User.create!(email: "a@a.com", password: "a")
 
+100.times do |i|
+  User.first.items.create!(address: address, name: "Item#{i}", categories: [Category.first])
+end
